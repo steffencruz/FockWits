b39b9f969a770e17070a1438e9cb6260cec758bf
Steffen Cruz <steffenc@triumf.ca>
2019-02-27 15:00:34 -0500

    unitary baby!

--- FockWit.py
+++ FockWit.py
@@ -3,6 +3,8 @@
 # squeezing
 
 import numpy as np
+from scipy.linalg import expm
+# import numpy.linalg.matrix_power as mat_pow
 
 class MultiMode():
 
@@ -24,37 +26,31 @@ class QMode():
         self.n_qubits_per_mode = n_qubits_per_mode
         self.n_dim = 2**n_qubits_per_mode
 
-    def a(self):
         # annihilation operator matrix for nxn
-        a = np.zeros((self.n_dim,self.n_dim))
+        self.a = np.zeros((self.n_dim,self.n_dim))
+
         for i in range(self.n_dim-1):
-            a[i,i+1]= np.sqrt(i+1)
-        return a
+            self.a[i,i+1]= np.sqrt(i+1)
 
-    def a_dag(self):
         # creation operator matrix for nxn
-        a = np.zeros((self.n_dim,self.n_dim))
-        for i in range(1,self.n_dim):
-            a[i,i-1]= np.sqrt(i)
-        return a
+        self.a_dag = self.a.conj().T
 
-    def N(self):
         # Number operator matrix for nxn
-        return np.matmul(a_dag(self.n_dim),a(self.n_dim))
+        self.N = np.matmul(self.a_dag,self.a)
 
     def D(self,alpha):
         # Displacement operator matrix for nxn
-        arg = alpha*a_dag(self.n_dim)-np.conjugate(alpha)*a(self.n_dim)
-        return np.expm1(arg)
+        arg = alpha*self.a_dag-np.conjugate(alpha)*self.a
+        return expm(arg)
 
     def S(self,z):
         #Single mode squeezing
-        a2 = np.matmul(a(self.n_dim),a(self.n_dim))
-        a2_dag = np.matmul(a_dag(self.n_dim),a_dag(self.n_dim))
+        a2 = np.matmul(self.a,self.a)
+        a2_dag = np.matmul(self.a_dag,self.a_dag)
         arg = np.conjugate(z)*a2 - z*a2_dag
-        return np.expm1(arg)
+        return expm(arg)
 
     def R(self,phi):
         j = np.complex(0,1)
-        arg = j*phi*np.matmul(a_dag(self.n_dim),a(self.n_dim))
-        return np.expm1(arg)
+        arg = j*phi*np.matmul(self.a_dag,self.a)
+        return expm(arg)
