cefded8c52abfe2cf5c5938a4427d1d6595d5445
Eric Brown <ericgb86@gmail.com>
2019-02-27 14:06:24 -0500

    all single mode gates

--- MotherFocker.py
+++ MotherFocker.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 
+# n is number of dimensions, which
 def a(n):
     # annihilation operator matrix for nxn
     a = np.zeros((n,n))
@@ -27,4 +28,16 @@ def N(n):
 def D(alpha,n):
     # Displacement operator matrix for nxn
     arg = alpha*a_dag(n)-np.conjugate(alpha)*a(n)
+    return np.expm1(arg)
+
+def S(z,n):
+    #Single mode squeezing
+    a2 = np.matmul(a(n),a(n))
+    a2_dag = np.matmul(a_dag(n),a_dag(n))
+    arg = np.conjugate(z)*a2 - z*a2_dag
+    return np.expm1(arg)
+
+def R(phi,n):
+    j = np.complex(0,1)
+    arg = j*phi*np.matmul(a_dag(n),a(n))
     return np.expm1(arg)
